f109349b7d17a942713713d31932114fb99902ac
Celian HAYDONT <chaydont@e2r8p8.42.fr>
2018-09-03 14:40:09 +0200

    Vector Class

---
+++ Vector2.cpp
@@ -0,0 +1,64 @@
+#include <math.h>
+#include "main.h"
+
+using namespace std;
+
+Vector2::Vector2(int c_x, int c_y)
+{
+	x = c_x;
+	y = c_y;
+}
+
+Vector2		Vector2::operator+=(Vector2 a)
+{
+	*this = *this + a;
+	return (*this);
+}
+
+Vector2		Vector2::operator-=(Vector2 a)
+{
+	*this = *this - a;
+	return (*this);
+}
+
+void		Vector2::print()
+{
+    cout << "X : " << x << endl << "Y : " << y << endl;
+}
+
+Vector2		operator+(Vector2 a, Vector2 b)
+{
+	Vector2 result;
+
+	result.x = a.x + b.x;
+	result.y = a.y + b.y;
+	return (result);
+}
+
+Vector2		operator-(Vector2 a, Vector2 b)
+{
+	Vector2 result;
+
+	result.x = a.x - b.x;
+	result.y = a.y - b.y;
+	return (result);
+}
+
+bool		operator==(Vector2 a, Vector2 b)
+{
+	return (a.x == b.x && a.y == b.y ? true : false);
+}
+
+bool		operator!=(Vector2 a, Vector2 b)
+{
+	return (a == b ? false : true);
+}
+
+double		distance(Vector2 a, Vector2 b)
+{
+	if (a == b)
+		return (0);
+	int distX = abs(a.x - b.x);
+	int distY = abs(a.y - b.y);
+	return (sqrt(distX * distX + distY * distY));
+}
---
+++ Vector2.hpp
@@ -0,0 +1,21 @@
+#ifndef VECTOR2_HPP
+# define VECTOR2_HPP
+
+class Vector2 {
+	public:
+		int x;
+		int y;
+
+	Vector2(int c_x = 0, int c_y = 0);
+	Vector2 operator+=(Vector2 a);
+	Vector2 operator-=(Vector2 a);
+	void	print();
+};
+
+	Vector2 operator+(Vector2 a, Vector2 b);
+	Vector2 operator-(Vector2 a, Vector2 b);
+	bool	operator==(Vector2 a, Vector2 b);
+	bool	operator!=(Vector2 a, Vector2 b);
+    double	distance(Vector2 a, Vector2 b);
+
+#endif
